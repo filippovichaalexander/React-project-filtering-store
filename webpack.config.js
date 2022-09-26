88cd879060225305392ad0d4014ad066e2726ddd
unknown <a.filippovich@gamesredrift.com>
2022-09-26 20:11:06 +0300

    5c

--- webpack.config.js
+++ webpack.config.js
@@ -24,12 +24,12 @@ module.exports = {
         use: ["style-loader", "css-loader", "sass-loader"]
       },
       {
-        test: /\.(png|jpe?g|gif)$/i,
-        loader: 'file-loader',
-        options: {
-          name: '[path][name].[ext]',
-        },
-      },
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          'url-loader?limit=10000',
+          'img-loader'
+        ]
+      }
     ]
   }
 };
